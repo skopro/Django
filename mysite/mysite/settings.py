45e996c440e0e628d6d6caf5e6e071a3cf5ccb2a
skopro <kavoosisoroosh@gmail.com>
2016-03-30 19:25:24 +0430

    Customize poll app’s look and feel
    css file:
    1. Change color of text in index.html
    2. Adding a background image
    
    I used the following command for static files but i don't know what is this exactly:
    python manage.py collectstatic

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'polls', 'static'),
+)
+
